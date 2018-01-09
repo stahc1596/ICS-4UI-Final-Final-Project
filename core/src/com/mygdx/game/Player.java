89bc7541ad062b3883ef526e583c1d41f8d4fb78
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2018-01-09 10:16:12 -0500

    Thinking of making the Tile class an abstract super class and make certain types of tiles in a sub class. That way the interact method is easier to program.

---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+/**
+ *
+ * @author stahc1596
+ */
+public class Player {
+
+    public Player(){
+        
+        }
+    
+    public boolean interact() {
+            return false;
+        }    
+}
--- core/src/com/mygdx/game/Tile.java
+++ core/src/com/mygdx/game/Tile.java
@@ -10,4 +10,20 @@ package com.mygdx.game;
  */
 public class Tile {
     
+    private Map[][] map;
+    private Screen[][] screen;
+    private boolean block;
+    private Player p1;
+    
+    public Tile(boolean block){
+        //Note that all tiles must either be programmed seperately or some other
+        //way. Don't leave them all false -Come back to this-
+        this.block = false;
+    }
+    //Check if the player has pressed the interact button
+    public void checkInteract(){
+        if(this.p1.interact()){
+            //Do something with the tile if able
+        }
+    }
 }
