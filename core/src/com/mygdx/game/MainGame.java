25f265c870dfaba41b8b5b39e6320bd5d2d5c32f
stahc1596 <stahc1596@HRH-IC0027342.wrdsb.ca>
2018-01-11 09:58:36 -0500

    Edited the screen class to make it easier to interact with. Might be unnecessary, if so, delete.

---
+++ core/src/com/mygdx/game/MainGame.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+/**
+ *
+ * @author stahc1596
+ */
+public class MainGame {
+    
+}
