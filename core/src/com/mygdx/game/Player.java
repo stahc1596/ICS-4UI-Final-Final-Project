ec0fbf24836523c64a39b00f3dabc63aca3ec194
messr2578 <messr2578@HRH-IC0027340.wrdsb.ca>
2018-01-12 10:01:51 -0500

    adding what i have so far

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -26,25 +26,26 @@ public class Player {
     private int distanceTraveledY;
     private int worldRow;
     private int worldColumn;
-    private Screen world;
-/**
- * 
- * @param x the players x position on the screen
- * @param y the players y position on the screen
- * @param row the row of the map where the player is
- * @param col the column of the map where the player is
- */
+    private MapScreen world;
+
+    /**
+     *
+     * @param x the players x position on the screen
+     * @param y the players y position on the screen
+     * @param row the row of the map where the player is
+     * @param col the column of the map where the player is
+     */
     public Player(float x, float y, int row, int col) {
         this.x = x;
         this.y = y;
-        this.world = new Screen();
+        this.world = new MapScreen();
         this.dx = 0;
         this.dy = 0;
-        this.worldRow =row;
+        this.worldRow = row;
         this.worldColumn = col;
         this.distanceTraveledX = 0;
         this.distanceTraveledY = 0;
-        
+
         this.player = new Texture("player.jpg");
     }
 
@@ -80,38 +81,45 @@ public class Player {
         } else {
             this.dy = 0;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
             String test = this.world.getTileType();
-            if(test.equals("Puzzle")){
-                puzzleInteract(place);
-            }else if(test.equals("Door")){
-                doorInteract(place);
+            if (test.equals("Puzzle")) {
+                puzzleInteract(this.world.getTile);
+            } else if (test.equals("Door")) {
+                doorInteract(this.world.getTile);
             }
         }
         this.x = this.x + this.dx;
         this.y = this.y + this.dy;
     }
-    public void setWorldRow(int row){   
+
+    public void setWorldRow(int row) {
         this.worldRow = row;
-    } 
-    public void setWorldCol(int col){   
+    }
+
+    public void setWorldCol(int col) {
         this.worldColumn = col;
-    } 
-    public void setScreen(Screen places){
+    }
+
+    public void setScreen(Screen places) {
         this.world = places;
     }
-    public float getPlayerX(){
+
+    public float getPlayerX() {
         return this.x;
     }
-    public float getPlayerY(){
+
+    public float getPlayerY() {
         return this.y;
     }
-    public int getWorldCol(){
+
+    public int getWorldCol() {
         return this.worldColumn;
     }
-    public int getWorldRow(){
+
+    public int getWorldRow() {
         return this.worldRow;
-    }   
+    }
 
     public String getDiretion() {
         if (this.directionX == 2 && this.directionY == 0) {
@@ -134,18 +142,15 @@ public class Player {
             return "error no direction found";
         }
     }
-    public void puzzleInteract(Tile puzzle){
-        if(puzzle == Tile ){
-            //interact
-            world.changePuzzleTile(this.x,this.y);
-        }
-        
+
+    public void puzzleInteract(String puzzle) {
+
+        //interact
+        world.changePuzzleTile(this.x, this.y);
     }
-    public void doorInteract(Tile Door){
-        if(Door){
-            //interact
-            world.changeMap(this.worldRow,this.worldColumn,this.x,this.y);
-        }
-        
+
+    public void doorInteract(Tile Door) {
+        //interact
+        world.changeMap(this.worldRow, this.worldColumn, this.x, this.y);
     }
 }
