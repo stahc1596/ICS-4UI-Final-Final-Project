202701bf9961af00e4e19f9ed5443b91f675f9bb
messr2578 <messr2578@HRH-IC0027340.wrdsb.ca>
2018-01-17 09:31:45 -0500

    i don't have anything else to go on

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -53,20 +53,62 @@ public class Player {
      * @param col the column of the map where the player is
      */
     public Player(float x, float y, int row, int col) {
+        // sets the income position
         this.x = x;
         this.y = y;
-        this.world = new MapScreen();
+
+        // player starts standing still
         this.dx = 0;
         this.dy = 0;
+
+        // no animation going on, so no time yet
+        this.elapsed = 0;
+
+        // load in the texture atlast to start finding pictures
+        this.atlas = new TextureAtlas("packed/player.atlas");
+
+        // finding the standing picture and load it in
+        this.standD = atlas.findRegion("standD");
+        // we create a new texture from the standing picture
+        // this creates a duplicate picture so we can flip it for the other direction
+        this.standL = new TextureRegion(standL);
+        this.standL.flip(true, false);
+
+        // create a run animation by finding every picture named run
+        // the atlas has an index from each picture to order them correctly
+        // this was done by naming the pictures in a certain way (run_1, run_2, etc.)
+        runR = new Animation(1f / 10f, atlas.findRegions("run"));
+
+        // load in the pictures from the atlas again, but we will flip them this time
+        Array<TextureAtlas.AtlasRegion> runLFrames = atlas.findRegions("run");
+        for (int i = 0; i < runLFrames.size; i++) {
+            // this is how we flip each image
+            runLFrames.get(i).flip(true, false);
+        }
+        // create the left animation
+        runL = new Animation(1f / 10f, runLFrames);
+
+        // start off by facing right
+        this.faceLeft = false;
+        // my collision rectangle is at the x,y value passed in
+        // it has the width and height of the standing picture
+        this.bounds = new Rectangle(x, y, standR.getRegionWidth(), standR.getRegionHeight());
         this.worldRow = row;
         this.worldColumn = col;
         this.distanceTraveledX = 0;
         this.distanceTraveledY = 0;
-
-        this.player = new Texture("player.jpg");
     }
-
-    public void update(boolean place) {
+    
+    public float getX(){
+        return x;
+    }
+    
+    public float getY(){
+        return y;
+    }
+    
+    
+    public void update(float deltaTime) {
         // movement
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             this.dx = 3;
@@ -97,7 +139,8 @@ public class Player {
             }
         } else {
             this.dy = 0;
-        }
+        }/**
+         *Replace getTileTyp with something else
         if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
             String test = this.world.getTile(this.x,this.y);
             if (test.equals("Puzzle")) {
@@ -106,10 +149,83 @@ public class Player {
                 doorInteract();
             }
         }
+        */
         this.x = this.x + this.dx;
         this.y = this.y + this.dy;
     }
 
+    public void fixCollision(Rectangle block) {
+        // are they colliding?
+        if (bounds.overlaps(block)) {
+            // calculate how much the are overlaping
+            float width = Math.min(bounds.x + bounds.width, block.x + block.width) - Math.max(bounds.x, block.x);
+            float height = Math.min(bounds.y + bounds.height, block.y + block.height) - Math.max(bounds.y, block.y);
+            // seperate the axis by finding the least amount of collision
+            if (width < height) {
+                // on the left
+                if (this.x < block.x) {
+                    // move the player to the left
+                    this.x = this.x - width;
+                // on the right
+                } else {
+                    // move the player to the right
+                    this.x = this.x + width;
+                }
+            } else {
+                // under it
+                if (this.y < block.y) {
+                    // move the player down
+                    this.y = this.y - height;
+                // above it
+                } else {
+                    // move the player up
+                    this.y = this.y + height;
+                }
+            }
+            // update the collision box to match the player
+            bounds.setX(this.x);
+            bounds.setY(this.y);
+        }
+    }
+
+    public void render(SpriteBatch batch){
+        //Check if the player is standing
+        if (this.dx == 0){
+            //Determine which direction the player is standing
+            //If the player is facing left
+            if(faceLeft){
+                batch.draw(standL, x, y);
+            //If the player is facing Right
+            }else if(faceRight){
+                batch.draw(standR, x, y);
+            //If the player is facing up
+            }else if(faceUp){
+                batch.draw(standU, x, y);
+            //If the player is facing down
+            }else{
+                batch.draw(standD, x, y);
+            }
+        //If the player is moving, one of the four animations play, even if
+        //they're moving diagonally
+        //If the player is moving to the right
+        }else if(this.dx > 0){
+            batch.draw(runR.getKeyFrame(elapsed, true), x, y);
+        //If the player is moving to the left
+        }else if(this.dx < 0){
+            batch.draw(runL.getKeyFrame(elapsed, true), x, y);
+        //If the player is moving upwards
+        }else if(this.dy < 0){
+            batch.draw(runU.getKeyFrame(elapsed, true), x, y);
+        //If the player is moving downwards
+        }else if(this.dy > 0){
+            batch.draw(runD.getKeyFrame(elapsed, true), x, y);
+        }
+    }
+    
+    // get rid of heavy objects
+    public void dispose(){
+        atlas.dispose();
+    }
     public void setWorldRow(int row) {
         this.worldRow = row;
     }
@@ -188,6 +304,6 @@ public class Player {
         //interact
         world.PassThroughDoor(this.worldRow, this.worldColumn, this.x, this.y);
     }
+    */
 }
 
-    
